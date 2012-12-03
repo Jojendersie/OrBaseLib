4bdb675710279b14caf98e1ddd190a17b4fa90d5
Johannes Jendersie <jojendersie@web.de>
2012-12-03 18:18:11 +0100

    + platform independent TypeDef for integers with fixed size

--- include/OrTypeDef.h
+++ include/OrTypeDef.h
@@ -17,17 +17,116 @@
 
 #pragma once
 
-typedef unsigned __int64 qword;
-typedef unsigned __int32 dword;
-typedef unsigned __int16 word;
-typedef unsigned __int8 byte;
+#include <limits.h>
+
+//typedef unsigned int64_t qword;
+//typedef unsigned int32_t dword;
+//typedef unsigned int16_t word;
+//typedef unsigned int8_t byte;
+
+#if(UCHAR_MAX == 0xff)
+	typedef unsigned char uint8;
+	typedef unsigned char byte;
+#else
+	#error "sizeof(char) is not 1"
+#endif
+
+#if(SCHAR_MAX == 0x7f)
+	typedef signed char int8;
+#else
+	#error "sizeof(char) is not 1"
+#endif
+
+#if(USHRT_MAX == 0xffff)
+	typedef unsigned short uint16;
+	typedef unsigned short word;
+#else
+	#if(UINT_MAX == 0xffff)
+		typedef unsigned int uint16;
+		typedef unsigned int word;
+	#else
+		#error "cannot support 16 bit unsigned integer on this plattform"
+	#endif
+#endif
+
+#if(SHRT_MAX == 0x7fff)
+	typedef signed short int16;
+#else
+	#if(INT_MAX == 0x7fff)
+		typedef signed int int16;
+	#else
+		#error "cannot support 16 bit signed integer on this plattform"
+	#endif
+#endif
+
+#if(USHRT_MAX == 0xffffffff)
+	typedef unsigned short uint32;
+	typedef unsigned short dword;
+#else
+	#if(UINT_MAX == 0xffffffff)
+		typedef unsigned int uint32;
+		typedef unsigned int dword;
+	#else
+		#if(ULONG_MAX == 0xffffffff)
+			typedef unsigned long uint32;
+			typedef unsigned long dword;
+		#else
+			#error "cannot support 32 bit unsigned integer on this plattform"
+		#endif
+	#endif
+#endif
+
+#if(SHRT_MAX == 0x7fffffff)
+	typedef signed short int32;
+#else
+	#if(INT_MAX == 0x7fffffff)
+		typedef signed int int32;
+		#else
+			#if(LONG_MAX == 0x7fffffff)
+			typedef signed long int32;
+		#else
+			#error "cannot support 32 bit signed integer on this plattform"
+		#endif
+	#endif
+#endif
+
+#if(ULLONG_MAX == 0xffffffffffffffff)
+	typedef unsigned long long uint64;
+	typedef unsigned long long qword;
+#else
+	#if(UINT_MAX == 0xffffffffffffffff)
+		typedef unsigned int uint64;
+		typedef unsigned int qword;
+	#else
+		#if(ULONG_MAX == 0xffffffffffffffff)
+			typedef unsigned long uint64;
+			typedef unsigned long qword;
+		#else
+			#error "cannot support 64 bit unsigned integer on this plattform"
+		#endif
+	#endif
+#endif
+
+#if(LLONG_MAX == 0x7fffffffffffffff)
+	typedef signed long long int64;
+#else
+	#if(INT_MAX == 0x7fffffffffffffff)
+		typedef signed int int64;
+		#else
+			#if(LONG_MAX == 0x7fffffffffffffff)
+			typedef signed long int64;
+		#else
+			#error "cannot support 64 bit signed integer on this plattform"
+		#endif
+	#endif
+#endif
 
 // ******************************************************************************** //
 // An integral type which has always the size of (void*)
 template <int BYTES> struct PtrToInt;
-template <> struct PtrToInt<2> {typedef word int_t;};
-template <> struct PtrToInt<4> {typedef dword int_t;};
-template <> struct PtrToInt<8> {typedef qword int_t;};
+template <> struct PtrToInt<2> {typedef uint16 int_t;};
+template <> struct PtrToInt<4> {typedef uint32 int_t;};
+template <> struct PtrToInt<8> {typedef uint64 int_t;};
 typedef PtrToInt<sizeof(void*)>::int_t uiptr;
 
 
